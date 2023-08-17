7ced36b5ff05f5a10bb92422386b860ddc383bf4
Rakesh Tripathi <tripathirakeshofficial@gmail.com>
2023-08-17 16:49:11 +0530

    HomePage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,11 @@ import { Inter } from "next/font/google";
 
 import "../globals.css";
 
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +24,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
